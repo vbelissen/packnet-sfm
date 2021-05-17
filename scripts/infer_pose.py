72909bd852e920b8ea5227601b826f75d7102581
Valentin Belissen <valentin.belissen@cea.fr>
2021-05-17 13:16:22 +0200

    infer pose

--- scripts/infer_pose.py
+++ scripts/infer_pose.py
@@ -147,8 +147,6 @@ def infer_and_save_pose(input_file_refs, input_file, model_wrapper, image_shape,
 
     # Depth inference (returns predicted inverse depth)
     pose_tensor = model_wrapper.pose(image, image_ref)[0][0]  # take the pose from 1st to 2nd image
-    print(model_wrapper.pose(image, image_ref))
-    print(model_wrapper.pose(image, image_ref)[0])
     print(model_wrapper.pose(image, image_ref)[0][0])
     rot_matrix = euler_angles_to_matrix(pose_tensor[3:], convention="ZYX")
     translation = pose_tensor[:3]

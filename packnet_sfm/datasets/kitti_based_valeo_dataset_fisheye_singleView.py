42183f1de3fbd96f6a19e37863ed98bcf314a36a
Valentin Belissen <valentin.belissen@cea.fr>
2021-05-19 10:48:49 +0200

    try other pose convention

--- packnet_sfm/datasets/kitti_based_valeo_dataset_fisheye_singleView.py
+++ packnet_sfm/datasets/kitti_based_valeo_dataset_fisheye_singleView.py
@@ -256,7 +256,7 @@ class KITTIBasedValeoDatasetFisheye_singleView(Dataset):
 
         R = np.matmul(Rz2, np.matmul(Rx, Rz1))
 
-        T_other_convention = -np.dot(R,t)
+        T_other_convention = t#-np.dot(R,t)
 
         pose_matrix = transform_from_rot_trans(R, T_other_convention).astype(np.float32)
         pose_matrix = invert_pose_numpy(pose_matrix)

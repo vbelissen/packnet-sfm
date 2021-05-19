5edfb7c22a0fe83ff8d666d275e9896cb3a931e1
Valentin Belissen <valentin.belissen@cea.fr>
2021-05-19 15:18:59 +0200

    try other pose convention

--- packnet_sfm/datasets/kitti_based_valeo_dataset_fisheye_singleView.py
+++ packnet_sfm/datasets/kitti_based_valeo_dataset_fisheye_singleView.py
@@ -660,7 +660,7 @@ class KITTIBasedValeoDatasetFisheye_singleView(Dataset):
                         self.calibration_cache[calib_identifier] = c_data
                     context_pose = self._get_extrinsics_pose_matrix(f, c_data)
                     invert_context_pose = invert_pose_numpy(context_pose)
-                    relative_pose = invert_context_pose @ first_pose
+                    relative_pose = invert_pose_numpy(invert_context_pose @ first_pose)
                     image_context_pose.append(relative_pose)
                     #else:
                     #    image_context_pose.append(None)

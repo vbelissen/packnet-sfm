45350b68cc5b5a86e934f38ebd392b3e6821fb26
Valentin Belissen <valentin.belissen@cea.fr>
2021-05-28 16:05:12 +0200

    test pose seulement contexte geo

--- packnet_sfm/models/SelfSupModel_fisheye_valeo_testPose3.py
+++ packnet_sfm/models/SelfSupModel_fisheye_valeo_testPose3.py
@@ -5,7 +5,7 @@ from packnet_sfm.losses.fisheye_multiview_photometric_loss_valeo_test3 import Mu
 from packnet_sfm.models.model_utils import merge_outputs
 
 
-class SelfSupModel_fisheye_valeo_testPose(SfmModel):
+class SelfSupModel_fisheye_valeo_testPose3(SfmModel):
     """
     Model that inherits a depth and pose network from SfmModel and
     includes the photometric loss for self-supervised training.

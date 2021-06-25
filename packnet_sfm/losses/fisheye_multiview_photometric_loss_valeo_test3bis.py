bbb339bd1d613c6202642976d720c5dc5511be68
Valentin Belissen <valentin.belissen@cea.fr>
2021-06-25 13:19:58 +0200

    modif contexte

--- packnet_sfm/losses/fisheye_multiview_photometric_loss_valeo_test3bis.py
+++ packnet_sfm/losses/fisheye_multiview_photometric_loss_valeo_test3bis.py
@@ -473,7 +473,7 @@ class MultiViewPhotometricLoss(LossBase):
                                                                                      pose_matrix_context[j],
                                                                                      pose)
                 photometric_loss = self.calc_photometric_loss([a * b * c for a, b, c in zip(ref_warped, ego_mask_tensors, ref_ego_mask_tensors_warped)],
-                                                              [a * b for a, b in zip(images,     ego_mask_tensors)])
+                                                              [a * b     for a, b    in zip(images,     ego_mask_tensors)])
             else:
                 ref_warped = self.warp_ref_image(inv_depths, ref_image,
                                                  path_to_theta_lut,        path_to_ego_mask,        poly_coeffs,        principal_point,        scale_factors,

b0a14807a399916c6d7b979f6ef993dcc8e373df
Valentin Belissen <valentin.belissen@cea.fr>
2021-07-13 11:48:07 +0200

    mask context new

--- packnet_sfm/losses/fisheye_multiview_photometric_loss_valeo_test3_contextDepth_v2.py
+++ packnet_sfm/losses/fisheye_multiview_photometric_loss_valeo_test3_contextDepth_v2.py
@@ -496,7 +496,7 @@ class MultiViewPhotometricLoss(LossBase):
                                                     same_timestep_as_origin[j],
                                                     pose_matrix_context[j],
                                                     pose)
-                    coeff_margin_occlusion = 2.0
+                    coeff_margin_occlusion = 1.5
                     without_occlusion_masks = [(inv_depths_wrt_ref_cam[i] <= coeff_margin_occlusion * ref_inv_depths_warped[i])
                                                 * (ref_inv_depths_warped[i] <= coeff_margin_occlusion * inv_depths_wrt_ref_cam[i])  for i in range(self.n)]
 

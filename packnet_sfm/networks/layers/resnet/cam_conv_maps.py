ebe617d28a8dd9666f1ebb15300de210471bf89f
Valentin Belissen <valentin.belissen@cea.fr>
2021-10-18 15:29:26 +0200

    test depthresnet

--- packnet_sfm/networks/layers/resnet/cam_conv_maps.py
+++ packnet_sfm/networks/layers/resnet/cam_conv_maps.py
@@ -9,9 +9,10 @@ import torch.nn as nn
 class CamConvMaps(nn.Module):
     def __init__(self):
         super(CamConvMaps, self).__init__()
+        self.num_maps = 7
 
     def forward(self, input_features):
         B, C, H, W = input_features.shape
         device = input_features.get_device()
-        output = torch.zeros(B, 7, H, W).to(device) # test
+        output = torch.zeros(B, self.num_maps, H, W).to(device) # test
         return output

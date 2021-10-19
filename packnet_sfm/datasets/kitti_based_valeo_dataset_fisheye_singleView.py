92d6bb0d46655b3fb1041b51ae78c6daade0083b
Valentin Belissen <valentin.belissen@cea.fr>
2021-10-19 21:07:59 +0200

    cam convs model test

--- packnet_sfm/datasets/kitti_based_valeo_dataset_fisheye_singleView.py
+++ packnet_sfm/datasets/kitti_based_valeo_dataset_fisheye_singleView.py
@@ -574,7 +574,7 @@ class KITTIBasedValeoDatasetFisheye_singleView(Dataset):
         theta_tensor[0] = torch.from_numpy(np.load(sample['path_to_theta_lut']))
         yi, xi = centered_2d_grid_loader(H, W, torch.from_numpy(principal_point), torch.from_numpy(scale_factors))
         sample.update({
-            'cam_features': torch.cat([theta_tensor, xi.squeeze().float(), yi.squeeze().float()], 1)
+            'cam_features': torch.cat([theta_tensor, xi.squeeze().float(), yi.squeeze().float()], 0)
         })
 
         # Add context information if requested

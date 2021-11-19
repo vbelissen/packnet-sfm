e6fb8196573693a81c0a6c723052be730e2117e4
Valentin Belissen <valentin.belissen@cea.fr>
2021-11-19 14:26:54 +0100

    try detach gradient in cam convs

--- packnet_sfm/models/SfmModel_fisheye_CamConvs.py
+++ packnet_sfm/models/SfmModel_fisheye_CamConvs.py
@@ -167,12 +167,12 @@ class SfmModel_fisheye_CamConvs(nn.Module):
         # Generate inverse depth predictions
 
         inv_depths = self.compute_inv_depths_with_cam(batch['rgb'],
-                                                      [x.detach() for x in batch['cam_features']])
+                                                      batch['cam_features'].detach())
         # Generate pose predictions if available
         pose = None
         if 'rgb_context' in batch and self.pose_net is not None:
             pose = self.compute_poses_with_cam(batch['rgb'],
-                                               [x.detach() for x in batch['cam_features']],
+                                               batch['cam_features'].detach(),
                                                batch['rgb_context'],
                                                [x.detach() for x in batch['cam_features_context']])
         # Return output dictionary
